8281ccfbb007228178b9dcbbc418ea5bb483a21f
kimdoyeob <ga.miro3721@gmail.com>
2025-08-05 14:38:31 +0900

    feat: 노래 목록 출력 기능 수정
    - 정적 리소스에서 API 통신으로 수정

--- src/app/lyrics/page.tsx
+++ src/app/lyrics/page.tsx
@@ -1,23 +1,22 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { songItems } from '../shared/songs';
+import { Song } from '../shared/songs';
 import Link from 'next/link';
+import { getApiUrl } from '@/app/shared/config';
 
-export default function LyricsPage() {
-  const router = useRouter();
+export default async function LyricsPage() {
+  const data = await fetch(getApiUrl('/songs'));
+  const json = await data.json();
+  const songs: Song[] = json._embedded.songs as Song[];
 
   return (
     <div className="max-w-2xl mx-auto w-full">
       <h2 className="text-2xl font-bold mb-6 text-left">가사</h2>
 
       <div className="flex flex-col gap-6 w-full">
-        {songItems.map((item, index) => (
+        {songs.map((item, index) => (
           <Link
             key={index}
             className="bg-gray-800 hover:bg-gray-900 text-white text-xl font-bold p-8 rounded-lg shadow-md transition-all transform hover:scale-105 focus:ring-4 focus:ring-gray-500 focus:outline-none cursor-pointer"
             href={`/lyrics/${item.id}`}
-            onClick={() => router.push(`/lyrics/${item.id}`)}
           >
             {item.title} - {item.artist}
           </Link>
--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -6,11 +6,11 @@
 /**
  * Base URL for API requests
  */
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL: string | undefined = process.env.API_BASE_URL;
 
 /**
  * Helper function to construct a full API URL
  * @param endpoint - The API endpoint to append to the base URL
  * @returns The full API URL
  */
-export const getApiUrl = (endpoint: string): string => `${API_BASE_URL}/${endpoint}`;
+export const getApiUrl = (endpoint: string): string => `${API_BASE_URL}${endpoint}`;
